648f780e3bbcf686e1fd13d590c12e393d397273
Usman-17 <usmandev580@gmail.com>
2024-08-28 23:27:32 +0500

    Set up database with MongoDB

--- backend/index.js
+++ backend/index.js
@@ -1,13 +1,17 @@
 import express from "express";
 import dotenv from "dotenv";
+import dbConnect from "./db/ConnectMongoDB.js";
 
 const app = express();
 dotenv.config();
 
-//* PORT Assign
+// PORT Assign
 const PORT = process.env.PORT || 9000;
 
-//* Running App
+
+
+// Running App
 app.listen(PORT, () => {
+  dbConnect();
   console.log(`Server is running on port ${PORT}`);
 });
